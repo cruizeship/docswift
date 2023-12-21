2e6ace789f8bfa430d127a9ae4dc66ca065b815d
Andrew Cruz <75159360+cruizeship@users.noreply.github.com>
2023-12-20 17:15:49 -0800

    Added Full Camera Functionality

--- screens/Camera.js
+++ screens/Camera.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import {
     StyleSheet,
     Text,
@@ -7,64 +7,219 @@ import {
     Image,
     TouchableOpacity,
     Button,
+    ScrollView,
 } from 'react-native'
 import { Camera as Camera2 } from 'expo-camera';
+import Icon from 'react-native-vector-icons/Feather';
 
 export default function Camera(props) {
     const { onRequestClose } = props;
 
     const [hasCameraPermission, setHasCameraPermission] = useState(null);
-    const [camera, setCamera] = useState(null);
-    const [image, setImage] = useState(null);
-    const [type, setType] = useState(Camera2.Constants.Type.back);useEffect(() => {
-    (async () => {
-      const cameraStatus = await Camera2.requestCameraPermissionsAsync();
-      setHasCameraPermission(cameraStatus.status === 'granted');})();
+    const [camera, setCamera] = useState(null)
+    //const [image, setImage] = useState([])
+    const [type, setType] = useState(Camera2.Constants.Type.back)
+    const [images, setImages] = useState([]);
+
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    const permisionFunction = async () => {
+        const cameraPermission = await Camera2.requestCameraPermissionsAsync();
+        setHasCameraPermission(cameraPermission.status === 'granted');
+    }
+
+    useEffect(() => {
+        permisionFunction();
     }, []);
 
     const takePicture = async () => {
         if (camera) {
             const data = await camera.takePictureAsync(null)
-            setImage(data.uri);
+            setImages([
+                ...images, { image: data.uri, selected: false } // Put old items at the end
+            ]);
         }
     }
+    
+    const popupMemo = useMemo(() => {
+        var objIndex = images.findIndex((obj => obj.selected == true));
+        if (objIndex != -1) {
+            return (
+                <View style={styles.popupContainer}>
+                    <TouchableOpacity style={{height: '100%', width: '100%', justifyContent: 'center', alignItems: 'center'}}
+                    onPress={() => {
+                        setIsPopupOpen(false)
+                    }}
+                    >
+                        <Image source={{uri: images[objIndex].image}} style={{alignSelf: 'center', borderWidth: 4, borderRadius: 5, borderColor: '#007AFF', height: 'undefined', width: '90%', aspectRatio: '3/4'}}/>
+                        <Text style={{marginTop: 10, color: 'white'}}>Tap Anywhere to Close</Text>
+                    </TouchableOpacity>
+                </View>
+            );
+        } else {
+            return (
+                <View style={styles.popupContainer}>
+                    <TouchableOpacity style={{height: '100%', width: '100%'}}
+                    onPress={() => {
+                        setIsPopupOpen(false)
+                    }}
+                    >
+                        
+                    </TouchableOpacity>
+                </View>
+            );
+        }
+    });
+
+    const selectImage = async (index) => {
+        if (images[index].selected == true) {
+            setIsPopupOpen(true)
+        }
+        var imagesCopy = [];
+        images.forEach((element) => {
+            element.selected = false;
+            imagesCopy.push(element)
+        });
+        imagesCopy[index].selected = true;
+        //images.forEach((element) => alert(element.image + ' ' + element.selected));
+        
+        setImages(imagesCopy);
+        //alert(index)
+    }
+
+    const deleteImage = async () => {
+
+        var objIndex = images.findIndex((obj => obj.selected == true));
+        if (objIndex == -1) {
+            alert('No Scan Selected')
+        }
+        
+        //images.forEach((element) => alert(element.image + ' ' + element.selected));
+        var imagesCopy = [];
+        images.forEach((element) => {
+            if (element.selected == false) {
+                imagesCopy.push(element)
+            }
+        });
+        setImages(imagesCopy);
+
+    }
+
+    const popupImage = async (index) => {
+        alert('long press')
+    }
+
     if (hasCameraPermission === false) {
-        return <Text>No access to camera</Text>;
+        return (
+            <View style={styles.container}>
+                <View style={[styles.noPermission, { flex: 4, width: '100%'}]}>
+                    <Text>No access to camera</Text>
+                </View>
+                <View style={[styles.tempContainer, { flex: 1, backgroundColor: 'white' }]}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            onRequestClose();
+                        }}
+                    >
+                        <Text style={{backgroundColor: 'lightblue', padding: 10}}>Click me to close</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
     }
 
     return (
         <View style={styles.container}>
-            <View style={[styles.tempContainer, { flex: 6, backgroundColor: 'lightblue' }]}>
-                <View style={{ flex: 1, width: '100%'}}>
+            <View style={[styles.tempContainer, { flex: 8 }]}>
+                <View style={{ flex: 4, width: '100%'}}>
                     <View style={styles.cameraContainer}>
                         <Camera2 
                         ref={ref => setCamera(ref)}
-                        style={styles.fixedRatio} 
-                        type={type}
-                        ratio={'1:1'} />
+                        style={styles.camera} 
+                        type={type} />
                     </View>
-                    <Button
-                    title="Flip Image"
+                    <TouchableOpacity
+                    style={[styles.flipCameraButton]}
                     onPress={() => {
                     setType(
                         type === Camera2.Constants.Type.back
                         ? Camera2.Constants.Type.front
                         : Camera2.Constants.Type.back
                     );
-                    }}></Button>
-                    <Button title="Take Picture" onPress={() => takePicture()} />
-                    {image && <Image source={{uri: image}} style={{flex:1}}/>}
+                    }}>
+                        <Icon name="refresh-cw" size={25} color="gray" style={{alignSelf: 'center' }}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.takePictureButton]} onPress={() => takePicture()}>
+                        <Icon name="camera" size={35} color="gray" style={{alignSelf: 'center' }}/>
+                    </TouchableOpacity>
+                </View>
+                <View style={[styles.middleContainer, {flex:1}]}>
+                    {/*image != null && (<View><Image source={{uri: image}} style={{width: 50, flex:1}}/>
+                    </View>)*/}
+                    {images.length != null && images.length != 0 && (
+                        <View style={{width: '100%', height: '100%'}}>
+                            <View pointerEvents="none" style={{ zIndex: 100, position: 'absolute', width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.2)', justifyContent: 'center', alignItems: 'center'}}>
+                                <Text style={{color: 'white'}}>Double Click Scan to Preview</Text>
+                            </View>
+                            <ScrollView style={{backgroundColor: 'lightgray', flex: 1, paddingLeft: 10, paddingRight: 10}} horizontal={true}>
+                                <View style={{flex: 1}}></View>
+                                <View style={styles.imagesContainer}>
+                                    {images.map((image, index) => (
+                                        <View key={index} style={[styles.image]}>
+                                            <TouchableOpacity
+                                                onPress={() => {
+                                                    selectImage(index);
+                                                }}
+                                            >
+                                            {image.selected && <Image source={{uri: image.image}} style={{borderWidth: 4, borderRadius: 5, borderColor: '#007AFF', width: '100%', height: '100%'}}/>}
+                                            {!image.selected && <Image source={{uri: image.image}} style={{ width: '100%', height: '100%'}}/>}
+                                            </TouchableOpacity>
+                                        </View>
+                                    ))}
+                                </View>
+                                <View style={{flex: 1}}></View>
+                            </ScrollView>
+                        </View>
+                    )}
+                    {images.length == null || images.length == 0 && (
+                        <View style={{flex: 1, backgroundColor: 'lightgray', justifyContent: 'center'}}>
+                            <Text style={{alignSelf: 'center', fontSize: 20}}>No Scans Selected</Text>
+                        </View>
+                    )}
                 </View>
             </View>
-            <View style={[styles.tempContainer, { flex: 1, backgroundColor: 'white' }]}>
-                <TouchableOpacity
-                    onPress={() => {
-                        onRequestClose();
-                    }}
-                >
-                    <Text style={{backgroundColor: 'lightblue', padding: 10}}>Click me to close</Text>
-                </TouchableOpacity>
+            <View style={[styles.bottomContainer, { flex: 1 }]}>
+                <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            onRequestClose();
+                        }}
+                    >
+                        <Text style={{ color: '#007AFF', fontSize: 18}}>Cancel</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={{flex: 2, alignItems: 'center', justifyContent: 'center'}}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            deleteImage();
+                        }}
+                    >
+                        <Icon name="trash-2" size={35} color="gray" style={{alignSelf: 'center'}}/>
+                    </TouchableOpacity>
+                </View>
+                <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            alert('Analyze');
+                        }}
+                    >
+                        <Text style={{ color: '#007AFF', fontSize: 18}}>Analyze</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
+            {isPopupOpen && (
+                <Modal animationType="fade" transparent={true}>{popupMemo}</Modal>
+            )}
         </View>
     )
 }
@@ -74,15 +229,74 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
     },
-    tempContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
+    bottomContainer: {
+        //alignItems: 'center',
+        //justifyContent: 'center',
+        flexDirection: 'row'
     },
     cameraContainer: {
         flex: 1,
         flexDirection: 'row'
     },
-    fixedRatio:{
+    camera:{
         flex: 1,
+    },
+    flipCameraButton: {
+        backgroundColor: 'white',
+        position: 'absolute',
+        height: '10%',
+        aspectRatio: '1/1',
+        bottom: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        left: 20,
+        borderRadius: 10000,
+        borderColor: 'gray',
+        borderWidth: 3
+    },
+    takePictureButton: {
+        backgroundColor: 'white',
+        position: 'absolute',
+        height: '15%',
+        aspectRatio: '1/1',
+        bottom: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        borderRadius: 10000,
+        borderColor: 'gray',
+        borderWidth: 5
+    },
+    noPermission: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+    },
+    middleContainer: {
+        width: '100%',
+        backgroundColor: 'lightgray',
+        flexDirection: 'row',
+        borderTopWidth: 3, 
+        borderBottomWidth: 3, 
+        borderColor: 'gray'
+    },
+    imagesContainer: {
+        width: 'fit-content',
+        backgroundColor: 'lightgray',
+        flexDirection: 'row',
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingRight: 10,
+    },
+    image: {
+        marginRight: 10,
+        height: '100%', 
+        width: 'undefined',
+        aspectRatio: '3/4',
+    },
+    popupContainer: {
+        height: '100%', 
+        width: '100%', 
+        backgroundColor: 'rgba(0,0,0,0.8)',
     },
 })
